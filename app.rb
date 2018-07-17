d7e014352409190dd8884133ddb966c153806261
Mary White <maryewhite2022@gmail.com>
2018-07-17 18:14:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,16 +14,3 @@ get 'fav-song'do
 end
 
 end
-
-
-require 'sinatra'
-
-class App < Sinatra::Base
-get '/' do
-  "Hello, World! Now I am changing this"
-end
-
-get '/Aspen' do 
-  "Hi my name is Aspen. "
-end
-end
