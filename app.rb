1d484526a82507668ec26868bf531253e29c5150
Mary White <maryewhite2022@gmail.com>
2018-07-17 18:14:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get 'name'do
+  "My name is Mary"
+end
+
+get 'hometown'do
+  "My hometown is LA"
+end
+
+get 'fav-song'do
+  "My fav hiphop song is party monster by the Weeknd"
+end
+
+end
+
+
+require 'sinatra'
+
+class App < Sinatra::Base
+get '/' do
+  "Hello, World! Now I am changing this"
+end
+
+get '/Aspen' do 
+  "Hi my name is Aspen. "
+end
 end
