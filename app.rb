800df8dd2b24d2d3cc7e85bc13d5ba620739918a
Mary White <maryewhite2022@gmail.com>
2018-07-17 18:18:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Mary"
+end
+
+get '/hometown'do
+  "My hometown is LA"
+end
+
+get '/favorite-song'do
+  "My favorite song is party monster by the Weeknd"
+end
+
 end
